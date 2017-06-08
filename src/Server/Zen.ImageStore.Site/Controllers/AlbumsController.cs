20bd3a1d7b387afc07bcbf8cf902826f01ef824c
DementedDevil <zen_global@hotmail.com>
2017-06-08 21:46:47 +0100

    renamed controllers

---
+++ src/Server/Zen.ImageStore.Site/Controllers/AlbumsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Zen.ImageStore.Site.Infrastructure;
+
+namespace Zen.ImageStore.Site.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlbumsController : Controller
+    {
+        private readonly IImageRepository _imageRepository;
+
+        public AlbumsController(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+
+        // GET: api/albums
+        [HttpGet]
+        [Route("")]
+        public async Task<IEnumerable<string>> GetAsync(CancellationToken cancellationToken)
+        {
+            return await _imageRepository
+                .ListImageContainersAsync(cancellationToken)
+                .ConfigureAwait(true);
+        }
+
+        // GET api/values/5
+        [HttpGet("{album}")]
+        public async Task<IEnumerable<string>> GetAlbumImagesAsync(
+            string album,
+            [FromQuery]string pathname,
+            CancellationToken cancellationToken)
+        {
+            var images = await _imageRepository
+                .ListImagesAsync(album, pathname, Guid.Empty, 5000, cancellationToken)
+                .ConfigureAwait(true);
+            return null;
+        }
+    }
+}
