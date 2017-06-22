0dcdea77f487e338674fa7824697ff112bbd3bb0
DementedDevil <zen_global@hotmail.com>
2017-06-22 01:35:06 +0100

    Adjustments needed to support latest version of bootstrap

--- src/Server/Zen.ImageStore.Site/Controllers/HomeController.cs
+++ src/Server/Zen.ImageStore.Site/Controllers/HomeController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Zen.ImageStore.Site.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
